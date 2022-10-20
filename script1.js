dca40dcb59fc1b90547724591180cc2822a089c7
kodaalec <113712502+kodaalec@users.noreply.github.com>
2022-10-20 13:29:13 -0600

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+let btnPrimary = document.querySelector("#primary");
+let btnDanger = document.querySelector("#danger");
+let btnSuccess = document.querySelector("#succes");
+
+btnPrimary.addEventListener(
+  "click",
+  () => (btnPrimary.style.backgroundColor = "#337ab7")
+);
+btnDanger.addEventListener(
+  "click",
+  () => (btnDanger.style.backgroundColor = "#c9302c")
+);
+btnSuccess.addEventListener(
+  "click",
+  () => (btnSuccess.style.backgroundColor = "#4cae4c")
+);
